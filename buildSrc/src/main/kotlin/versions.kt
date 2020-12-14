587051e87a5a5dded38b1b2582c50b226ca183e0
andylamax <andylamax@programmer.net>
2020-12-14 12:29:41 +0300

    Initial commit

---
+++ buildSrc/src/main/kotlin/versions.kt
@@ -0,0 +1,10 @@
+object vers {
+    val kotlin = "1.4.20"
+    val nexus_staging = "0.22.0"
+
+    object asoft {
+        val contacts = "0.0.1"
+        val builders = "1.1.1"
+        val test = "1.0.1"
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        jcenter()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.namespace == "com.android") {
+                useModule("com.android.tools.build:gradle:${requested.version}")
+            }
+        }
+    }
+}
+
+rootProject.name = "contacts"
+include(":email-core")
+include(":phone-core")
+include(":name-core")
